4f8edc00e45a0a888ae8417363d2c45cc2355be5
notcl4y14 <notcl4y@gmail.com>
2025-04-03 10:32:44 +0300

    Add Attribute, Attribute List

--- include/DL/DL.h
+++ include/DL/DL.h
@@ -11,13 +11,50 @@
 #define DL_TYPE_FLOAT 0x05
 #define DL_TYPE_DOUBLE 0x06
 
+// TODO: Make structs like DLAttr and DLAttrList private if needed
+struct DLAttr;
+struct DLAttrList;
+
 struct DLTexture;
 struct DLShader;
 struct DLPath;
 
+typedef struct DLAttr DLAttr;
+typedef struct DLAttrList DLAttrList;
+
 typedef struct DLTexture DLTexture;
 typedef struct DLShader DLShader;
 typedef struct DLPath DLPath;
+
+struct DLAttr {
+	void*   value;
+	uint8_t value_type;
+	size_t  value_size;
+	size_t  value_stride; // Used for splitting values
+};
+
+void DL_InitAttr (DLAttr* attr, uint8_t type, size_t size, size_t stride);
+void DL_DeleteAttr (DLAttr* attr);
+
+void DL_ReinitAttr (DLAttr* attr, uint8_t type, size_t size, size_t stride);
+
+void DL_AttrGetValue (DLAttr* attr, void* to);
+void DL_AttrSetValue (DLAttr* attr, void* from);
+
+struct DLAttrList {
+	DLAttr*  data_v;
+	char**   data_k;
+	uint32_t data_capacity;
+};
+
+void DL_InitAttrList (DLAttrList* attrlist, uint32_t data_capacity);
+void DL_DeleteAttrList (DLAttrList* attrlist);
+
+DLAttr* DL_AttrListGetAttr (DLAttrList* attrlist, uint32_t location);
+char*   DL_AttrListGetAttrID (DLAttrList* attrlist, uint32_t location);
+
+uint32_t DL_AttrListGetAttrLocation (DLAttrList* attrlist, char* id);
+void     DL_AttrListBindAttrLocation (DLAttrList* attrlist, uint32_t location, char* id);
 
 struct DLTexture {
 	void*   data;
@@ -50,18 +87,36 @@ void DL_TextureUsePath (DLTexture* texture, DLPath* path);
 
 struct DLShader {
 	void (*func) (uint32_t coord[2], void* color);
+	DLAttrList attrlist;
 };
 
 DLShader* DL_CreateShader ();
 DLShader  DL_CreateShaderP ();
 void DL_DeleteShader (DLShader* shader);
 
+void DL_InitShaderAttrList (DLShader* shader, uint32_t capacity);
+uint32_t DL_GetShaderAttrLocation (DLShader* shader, char* id);
+void DL_BindShaderAttrLocation (DLShader* shader, uint32_t location, char* id);
+
+void DL_InitShaderAttr (DLShader* shader, uint32_t location, uint8_t type, size_t size, size_t stride);
+void DL_GetShaderAttrValue (DLShader* shader, void* to, uint32_t location);
+void DL_SetShaderAttrValue (DLShader* shader, void* from, uint32_t location);
+
 struct DLPath {
 	void (*func) (DLTexture* texture);
+	DLAttrList attrlist;
 };
 
 DLPath* DL_CreatePath ();
 DLPath  DL_CreatePathP ();
 void DL_DeletePath (DLPath* path);
+
+void DL_InitPathAttrList (DLPath* path, uint32_t capacity);
+uint32_t DL_GetPathAttrLocation (DLPath* path, char* id);
+void DL_BindPathAttrLocation (DLPath* path, uint32_t location, char* id);
+
+void DL_InitPathAttr (DLPath* path, uint32_t location, uint8_t type, size_t size, size_t stride);
+void DL_GetPathAttrValue (DLPath* path, void* to, uint32_t location);
+void DL_SetPathAttrValue (DLPath* path, void* from, uint32_t location);
 
 #endif
--- source/DL.c
+++ source/DL.c
@@ -3,6 +3,93 @@
 #include <stdlib.h>
 #include <string.h>
 
+void DL_InitAttr (DLAttr* attr, uint8_t type, size_t size, size_t stride) {
+	attr->value = calloc(size, 1);
+	attr->value_type = type;
+	attr->value_size = size;
+	attr->value_stride = stride;
+}
+
+void DL_DeleteAttr (DLAttr* attr) {
+	free(attr->value);
+}
+
+void DL_ReinitAttr (DLAttr* attr, uint8_t type, size_t size, size_t stride) {
+	DL_DeleteAttr(attr);
+	DL_InitAttr(attr, type, size, stride);
+}
+
+void DL_AttrGetValue (DLAttr* attr, void* to) {
+	memcpy(to, attr->value, attr->value_size);
+}
+
+void DL_AttrSetValue (DLAttr* attr, void* from) {
+	memcpy(attr->value, from, attr->value_size);
+}
+
+void DL_InitAttrList (DLAttrList* attrlist, uint32_t data_capacity) {
+	attrlist->data_v = calloc(data_capacity, sizeof(DLAttr));
+	attrlist->data_k = calloc(data_capacity, sizeof(char*));
+	attrlist->data_capacity = data_capacity;
+}
+
+void DL_DeleteAttrList (DLAttrList* attrlist) {
+	uint32_t loop_loc = -1;
+
+	while (++loop_loc < attrlist->data_capacity) {
+		DLAttr* attr = DL_AttrListGetAttr(attrlist, loop_loc);
+
+		if (attr->value == NULL) {
+			continue;
+		}
+
+		DL_DeleteAttr(attr);
+	}
+
+	loop_loc = -1;
+
+	while (++loop_loc < attrlist->data_capacity) {
+		char* id = DL_AttrListGetAttrID(attrlist, loop_loc);
+
+		if (id == NULL) {
+			continue;
+		}
+
+		free(id);
+	}
+
+	free(attrlist->data_v);
+	free(attrlist->data_k);
+}
+
+DLAttr* DL_AttrListGetAttr (DLAttrList* attrlist, uint32_t location) {
+	return &attrlist->data_v[location];
+}
+
+char* DL_AttrListGetAttrID (DLAttrList* attrlist, uint32_t location) {
+	return attrlist->data_k[location];
+}
+
+uint32_t DL_AttrListGetAttrLocation (DLAttrList* attrlist, char* id) {
+	uint32_t location = -1;
+
+	while (++location < attrlist->data_capacity) {
+		char* comp_id = DL_AttrListGetAttrID(attrlist, location);
+
+		if (strcmp(id, comp_id) == 0) {
+			return location;
+		}
+	}
+
+	return -1;
+}
+
+void DL_AttrListBindAttrLocation (DLAttrList* attrlist, uint32_t location, char* id) {
+	size_t id_size = strlen(id);
+	attrlist->data_k[location] = malloc(id_size);
+	memcpy(attrlist->data_k[location], id, id_size);
+}
+
 DLTexture* DL_CreateTexture (uint32_t width, uint32_t height, uint8_t unit_type, size_t unit_size) {
 	DLTexture* texture = malloc(sizeof(DLTexture));
 
@@ -78,7 +165,7 @@ void DL_ResizeTexture (DLTexture* texture, uint32_t width, uint32_t height) {
 	// TODO: Finish this function
 }
 
-void DL_TextureUseShader (DLTexture* texture, DLShader* shader) {
+void DL_TextureUseShader (DLTexture* texture, DLShader* path) {
 	uint32_t loop_loc = -1;
 
 	while (++loop_loc < texture->data_capacity)
@@ -87,7 +174,7 @@ void DL_TextureUseShader (DLTexture* texture, DLShader* shader) {
 		uint32_t coordY = loop_loc / texture->width;
 		uint32_t coord[2] = {coordX, coordY};
 		void*    color = DL_TextureGetPixelPI(texture, loop_loc);
-		shader->func(coord, color);
+		path->func(coord, color);
 	}
 }
 
@@ -97,15 +184,42 @@ void DL_TextureUsePath (DLTexture* texture, DLPath* path)
 }
 
 DLShader* DL_CreateShader () {
-	DLShader* shader = malloc(sizeof(DLShader));
+	DLShader* path = malloc(sizeof(DLShader));
 
-	shader->func = NULL;
+	path->func = NULL;
 
-	return shader;
+	return path;
 }
 
-void DL_DeleteShader (DLShader* shader) {
+void DL_DeleteShader (DLShader* path) {
 	return;
+}
+
+void DL_InitShaderAttrList (DLShader* path, uint32_t capacity) {
+	DL_InitAttrList(&path->attrlist, capacity);
+}
+
+uint32_t DL_GetShaderAttrLocation (DLShader* path, char* id) {
+	return DL_AttrListGetAttrLocation(&path->attrlist, id);
+}
+
+void DL_BindShaderAttrLocation (DLShader* path, uint32_t location, char* id) {
+	DL_AttrListBindAttrLocation(&path->attrlist, location, id);
+}
+
+void DL_InitShaderAttr (DLShader* path, uint32_t location, uint8_t type, size_t size, size_t stride) {
+	DLAttr* attr = DL_AttrListGetAttr(&path->attrlist, location);
+	DL_InitAttr(attr, type, size, stride);
+}
+
+void DL_GetShaderAttrValue (DLShader* path, void* to, uint32_t location) {
+	DLAttr* attr = DL_AttrListGetAttr(&path->attrlist, location);
+	DL_AttrGetValue(attr, to);
+}
+
+void DL_SetShaderAttrValue (DLShader* path, void* from, uint32_t location) {
+	DLAttr* attr = DL_AttrListGetAttr(&path->attrlist, location);
+	DL_AttrSetValue(attr, from);
 }
 
 DLPath* DL_CreatePath () {
@@ -118,4 +232,31 @@ DLPath* DL_CreatePath () {
 
 void DL_DeletePath (DLPath* path) {
 	return;
+}
+
+void DL_InitPathAttrList (DLPath* path, uint32_t capacity) {
+	DL_InitAttrList(&path->attrlist, capacity);
+}
+
+uint32_t DL_GetPathAttrLocation (DLPath* path, char* id) {
+	return DL_AttrListGetAttrLocation(&path->attrlist, id);
+}
+
+void DL_BindPathAttrLocation (DLPath* path, uint32_t location, char* id) {
+	DL_AttrListBindAttrLocation(&path->attrlist, location, id);
+}
+
+void DL_InitPathAttr (DLPath* path, uint32_t location, uint8_t type, size_t size, size_t stride) {
+	DLAttr* attr = DL_AttrListGetAttr(&path->attrlist, location);
+	DL_InitAttr(attr, type, size, stride);
+}
+
+void DL_GetPathAttrValue (DLPath* path, void* to, uint32_t location) {
+	DLAttr* attr = DL_AttrListGetAttr(&path->attrlist, location);
+	DL_AttrGetValue(attr, to);
+}
+
+void DL_SetPathAttrValue (DLPath* path, void* from, uint32_t location) {
+	DLAttr* attr = DL_AttrListGetAttr(&path->attrlist, location);
+	DL_AttrSetValue(attr, from);
 }
